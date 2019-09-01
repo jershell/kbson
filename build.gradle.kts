c5818fc97536e8c7b209e2b8a97df73d30ddbb76
d.tukhvatullin <tukhvatullin01@gmail.com>
2019-09-01 18:18:57 +0300

    up version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import java.util.Date
 
-val LIBRARY_VERSION_NAME = "0.0.3"
+val LIBRARY_VERSION_NAME = "0.0.4"
 val GROUP_ID = "com.github.jershell"
 val ARTIFACT_ID = rootProject.name
 val BINTRAY_REPOSITORY = "generic"
