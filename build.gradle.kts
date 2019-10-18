80105bde6c7c8e021448314a2ca2b7a8ec07abda
d.tukhvatullin <tukhvatullin01@gmail.com>
2019-10-18 18:03:21 +0300

    prepare to publish maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 import java.util.Date
 
-val LIBRARY_VERSION_NAME = "0.1.2"
+val LIBRARY_VERSION_NAME = "0.1.3"
 val GROUP_ID = "com.github.jershell"
 val ARTIFACT_ID = rootProject.name
 val BINTRAY_REPOSITORY = "generic"
 val BINTRAY_ORGINIZATION = "jershell"
 val KOTLINX_SERIALIZATION_RUNTIME = "0.11.1"
+val SHORT_DESC = """
+    This adapter adds BSON support to kotlinx.serialization.
+""".trimIndent()
+val VCS_URL = "https://github.com/jershell/kbson.git"
+val WEBSITE_URL = "https://github.com/jershell/kbson"
+val ISSUE_TRACKER_URL = "https://github.com/jershell/kbson/issues"
+val CONTACT_EMAIL = "jershell@mail.ru"
 
 buildscript {
     repositories { jcenter() }
@@ -48,6 +55,28 @@ publishing {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
             artifact(tasks["sourcesAll"])
+            pom {
+                name.set(provider { "$GROUP_ID:$ARTIFACT_ID" })
+                description.set(provider { project.description ?: SHORT_DESC  })
+                url.set(VCS_URL)
+                developers {
+                    developer {
+                        name.set(BINTRAY_ORGINIZATION)
+                        email.set(CONTACT_EMAIL)
+                    }
+                }
+                scm {
+                    connection.set(VCS_URL)
+                    developerConnection.set(VCS_URL)
+                    url.set(ISSUE_TRACKER_URL)
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+            }
         }
     }
 }
@@ -63,7 +92,7 @@ bintray {
         repo = BINTRAY_REPOSITORY
         name = ARTIFACT_ID
         userOrg = BINTRAY_ORGINIZATION
-        desc = "This adapter adds BSON support to kotlinx.serialization.\n"
+        desc = SHORT_DESC
         setLicenses("MIT")
         version.apply {
             name = LIBRARY_VERSION_NAME
@@ -71,9 +100,9 @@ bintray {
             released = Date().toString()
         }
 
-        vcsUrl = "https://github.com/jershell/kbson.git"
-        websiteUrl = "https://github.com/jershell/kbson"
-        issueTrackerUrl = "https://github.com/jershell/kbson/issues"
+        vcsUrl = VCS_URL
+        websiteUrl = WEBSITE_URL
+        issueTrackerUrl = ISSUE_TRACKER_URL
 
         setLabels("kotlin", "bson", "serialization", "jvm", "mongo", "mongodb", "kmongo")
     }
